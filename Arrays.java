c77091a9c9e9ff5bd602b0d2ba5b4799f06e0b2f
Jenisha J <jenishajamesxavier@gmail.com>
2025-07-28 21:47:03 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
